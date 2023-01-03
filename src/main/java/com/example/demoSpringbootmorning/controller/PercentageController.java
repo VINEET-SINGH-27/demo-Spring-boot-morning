fa21ab2590c8e29e5b1e4028dd9b7c8848332193
VIN07-27 <vs1114466@gmail.com>
2023-01-03 14:53:11 +0530

    Day-2 of Controller project

---
+++ src/main/java/com/example/demoSpringbootmorning/controller/PercentageController.java
@@ -0,0 +1,17 @@
+package com.example.demoSpringbootmorning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PercentageController {
+
+    @RequestMapping("/percentage")
+    public String percent() {
+        int num = 453;
+        double percent = 15;
+
+        double percentage = (num*percent)/100;
+        return "Percentage of "+num+" is = "+percentage;
+    }
+}
