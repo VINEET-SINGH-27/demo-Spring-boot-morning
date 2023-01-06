5cdc0b6e47f72fed68b1bd0e8ca0633cabab4116
VIN07-27 <vs1114466@gmail.com>
2023-01-06 09:57:36 +0530

    Demo Controller

---
+++ src/main/java/com/example/demoSpringbootmorning/controller/CopyController.java
@@ -0,0 +1,25 @@
+package com.example.demoSpringbootmorning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CopyController {
+
+
+    int i = 1;
+    @RequestMapping("/hello")  // REQUEST MAPPING
+    public void hello () {
+
+        System.out.println("Hello , Spring boot");
+    }
+
+    @RequestMapping("/welcome")  // request mapping
+    public String massage () {
+
+
+        String welcome = "Welcome to Spring boot"+i;
+        i++;
+        return welcome;
+    }
+}
